a877b0d328eb9762824c49c0e7f884ac80862532
Ch3mie <ryanc668@nycstudents.net>
2024-11-25 12:00:51 -0500

    e

--- app/JS/main.js
+++ app/JS/main.js
@@ -17,7 +17,12 @@ async function getData() {
 
     data.items.forEach((character) => {
       console.log(character.name);
-      createcards(character.image, character.race, character.name);
+      createcards(
+        character.image,
+        character.race,
+        character.name,
+        character.affiliation
+      );
     });
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -26,7 +31,7 @@ async function getData() {
 }
 
 getData();
-function createcards(img, desc, title) {
+function createcards(img, race, title, desc) {
   DOMselectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card bg-base-100 w-96 h-120 shadow-xl flex flex-col justify-between items-center">
@@ -35,9 +40,10 @@ function createcards(img, desc, title) {
       </figure>
       <div class="card-body text-center flex-1 flex flex-col justify-center items-center">
         <h2 class="card-title text-xl mb-4">${title}</h2>
+        <p class="text-lg mb-4">${race}</p>
         <p class="text-lg mb-4">${desc}</p>
         <div class="card-actions flex justify-center w-full">
-          <button class="btn btn-primary w-full">Learn More</button>
+          <button class="btn btn-primary w-full" id="learn">Learn More</button>
         </div>
       </div>
     </div>`

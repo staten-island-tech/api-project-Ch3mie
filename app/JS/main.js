91fbab68a68ed94d26dde9ea468c77f6e62ce850
Ch3mie <ryanc668@nycstudents.net>
2024-12-03 11:06:20 -0500

    w

--- app/JS/main.js
+++ app/JS/main.js
@@ -13,7 +13,6 @@ async function getData() {
 
     const data = await response.json();
     call2(data);
-    console.log(data);
 
     data.items.forEach((character) => {
       createcards(
@@ -42,7 +41,7 @@ function createcards(img, race, title, desc, id) {
         <p class="text-lg mb-4">${race}</p>
         <p class="text-lg mb-4">${desc}</p>
         <div class="card-actions flex justify-center w-full">
-          <button class="btn btn-primary w-full" id="learn" data-id="${id}">Learn More</button>
+          <button class="btn btn-primary w-full" id="${id}">Learn More</button>
         </div>
       </div>
     </div>`
@@ -55,16 +54,50 @@ function reset() {
 
 function call2(data) {
   DOMselectors.container.addEventListener("click", (event) => {
-    if (event.target && event.target.id === "learn") {
+    if (event.target.nodeName === "BUTTON") {
       event.preventDefault();
       const apiData = data.items;
+      console.log(event.target.id);
       const selectedcharacter = apiData.find(
         (character) => character.id == event.target.id
       );
       console.log("You did it! :D");
-      if (selectedcharacter) {
-        reset();
-      }
+      reset();
+      DOMselectors.container.insertAdjacentHTML(
+        "beforeend",
+        `<div class="card bg-base-100 w-96 h-120 shadow-xl flex flex-col justify-between items-center">
+          <figure class="flex justify-center h-72 w-full overflow-hidden">
+            <img src="${selectedcharacter.image}" alt="${selectedcharacter.affiliation}" class="rounded-xl object-contain w-full h-full" />
+          </figure>
+          <div class="card-body text-center flex-1 flex flex-col justify-center items-center">
+            <h2 class="card-title text-xl mb-4">${selectedcharacter.name}</h2>
+            <p class="text-lg mb-4">${selectedcharacter.race}</p>
+            <p class="text-lg mb-4">${selectedcharacter.affiliation}</p>
+            <div class="card-actions flex justify-center w-full">
+            <button class="btn btn-primary w-full">Go Back</button>
+            </div>
+          </div>
+        </div>`
+      );
+      headBack(apiData);
+    }
+  });
+}
+
+function headBack(data) {
+  DOMselectors.container.addEventListener("click", (event) => {
+    if (event.target.nodeName === "BUTTON") {
+      event.preventDefault();
+      console.log(data);
+      data.items.forEach((character) => {
+        createcards(
+          character.image,
+          character.race,
+          character.name,
+          character.affiliation,
+          character.id
+        );
+      });
     }
   });
 }
